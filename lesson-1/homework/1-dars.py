007c89ccdfaeb0ce215b5ccdbfbdb92dfdb11b87
shokirruziev <shokirruziev95@gmail.com>
2025-08-15 18:05:47 +0500

    Update 1-dars.py

--- lesson-1/homework/1-dars.py
+++ lesson-1/homework/1-dars.py
@@ -1,2 +1,15 @@
-country = "Uzbekistan"
-print(country)
+    # 2  aylana uzunligi
+na=int(input("diametrni kiriting"))
+print(na*3.14)
+    # 3 kvadrat perimetri va yuzi
+side=5
+print("perimetr", side*4)
+print("yuzi",side**2)
+     # 3 ikki son o`rtachasi
+aa=15
+bb=25
+print((aa+bb)/2)
+    # yig`indi ko`paytma, kvadrat
+print(aa+bb)
+print(aa*bb)
+print(aa**bb)

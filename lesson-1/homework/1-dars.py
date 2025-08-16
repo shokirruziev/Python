8174efe3d8f838c8e65d5d8e333f4655eba8473b
shokirruziev <shokirruziev95@gmail.com>
2025-08-16 13:53:49 +0500

    Update 1-dars.py

--- lesson-1/homework/1-dars.py
+++ lesson-1/homework/1-dars.py
@@ -1,15 +1,22 @@
-    # 2  aylana uzunligi
-na=int(input("diametrni kiriting"))
-print(na*3.14)
-    # 3 kvadrat perimetri va yuzi
-side=5
-print("perimetr", side*4)
-print("yuzi",side**2)
-     # 3 ikki son o`rtachasi
-aa=15
-bb=25
-print((aa+bb)/2)
-    # yig`indi ko`paytma, kvadrat
-print(aa+bb)
-print(aa*bb)
-print(aa**bb)
+# 1 Kvadrtnining bir tomoni berilgan uning peremetri va yuzasini toping
+
+a = 4
+p = 4 * a
+s = p * 2
+print('peremetr',p)
+print('yuzi',s)
+
+# 2  aylana uzunligi
+a = int(input('radiusni kirit'))
+print(a*2*3.14)
+# 3 sonlar ortachasi
+a = 80
+b = 50
+print((a+b)/2)
+# 4 sonlar yigindisi,kopaytmasi,har bir son kvadrati
+a = 80
+b = 50
+print(a+b)
+print(a*b)
+print(a**2)
+print(b**2)
